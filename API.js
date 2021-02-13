54d1c3c80e3b4b18838ec8ba5fdf5716007106eb
drlocke <marcel.schulz93@googlemail.com>
2021-02-13 13:51:53 +0100

    Update API.js

--- API.js
+++ API.js
@@ -9,17 +9,22 @@ async function getIzidenzFromAPI(region, htmlValue) {
 	var icidenzNumber = "unknown";
 	var counter = 100;
 	do {
-		let request = new XMLHttpRequest();
-		request.open("GET", "https://api.corona-zahlen.org/districts/" + region, false);
-		request.send(null);
-		if (request.status == 200) {
-			var icidenzNumber = JSON.parse(request.responseText)['data'][region]['weekIncidence'];
-			if (icidenzNumber > 50.00) {
-				document.getElementById(htmlValue).style.color = "red";
+		try {
+			let request = new XMLHttpRequest();
+			request.open("GET", "https://api.corona-zahlen.org/districts/" + region, false);
+			request.send(null);
+			if (request.status == 200) {
+				var icidenzNumber = JSON.parse(request.responseText)['data'][region]['weekIncidence'];
+				if (icidenzNumber > 50.00) {
+					document.getElementById(htmlValue).style.color = "red";
+				}
+				icidenzNumber = icidenzNumber.toFixed(2);
+				console.debug("icidenzNumber: " + icidenzNumber);
+			} else {
+				await Sleep(100);
 			}
-			icidenzNumber = icidenzNumber.toFixed(2);
-			console.debug("icidenzNumber: " + icidenzNumber);
-		} else {
+		} catch (e) {
+			console.debug("Error: " + e);
 			await Sleep(100);
 		}
 		counter--;
